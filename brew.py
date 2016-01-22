70adb21b42f7d20ff7ebb721b1726e59149187a0
Kristian Wiklund <kristianwiklund@users.noreply.github.com>
2016-01-22 23:12:57 +0000

    basic gui

--- brew.py
+++ brew.py
@@ -1,25 +1,77 @@
 import curses
 import time
-import brewtroller
+from brewtroller import *
 
 class Tun:
     # window size
-    height=10
-    width=10
+    height=13
+    width=11
 
     # temperature display position in the window
     tempx=3
     tempy=3
 
-    def __init__(self,myid,x,y):
+    def __init__(self,bt,myid,title,x,y):
         self.id = myid
         self.win = curses.newwin(self.height, self.width, x, y)
         self.win.border("|","|","-","-","/","\\","\\","/")
+        self.win.addstr(0,(self.width-len(title))/2,title)
+        self.win.addstr(2,3,"Temp:",curses.A_BOLD)
+        self.win.addstr(5,3,"Set:",curses.A_BOLD)
+        self.win.addstr(8,3,"Power:",curses.A_BOLD)
+        self.bt = bt
 
     def update(self):
         # get temperature
+        temperature=self.bt.getTemp(self.id)
+        self.temperature = temperature
+
+        if temperature > 150: # easy, ugly check if there is no sensor
+            self.win.addstr(3,3,"ERROR",curses.A_BLINK)
+        else:
+            self.win.addstr(3,3,"     ") # erase first
+            self.win.addstr(3,3,str(temperature))
+
+        # get setpoint
+
+        setpoint=self.bt.getSetpoint(self.id)
+        self.setpoint=setpoint
+        self.win.addstr(6,3,"     ") # erase first
+        self.win.addstr(6,3,str(setpoint))
+
+        # get heatingpower
+
+        power=self.bt.getHeatpwr(self.id)
+        self.power=power
+        self.win.addstr(9,3,"     ") # erase first
+        self.win.addstr(9,3,str(power)+"%")
+
         self.win.refresh()
 
+class Pump:
+    state=0 # 0 - recirc, 1 - fill 
+    
+    def __init__(self,x,y,width,height):
+        self.win =  curses.newwin(height, width, y, x)
+        #self.win.hline(10,0,"=",width)
+        #self.win.border()
+        self.width=width
+        self.height=height
+
+    def update(self):
+        # here is code to check the state of the valves
+        # if newstate != oldstate then redraw...
+
+        self.win.erase()
+        
+        if self.state == 0:
+            self.win.hline(10,0,">",2)
+            self.win.hline(2,0,"<",2)
+            self.win.vline(2,2,"^",8)
+        else:
+            self.win.hline(10,0,">",self.width)
+
+        self.win.refresh()
 
 def main(stdscr):
 #    begin_x = 20; begin_y = 7
@@ -33,9 +85,15 @@ def main(stdscr):
     bt = BrewTroller("http://10.168.0.10/cgi-bin/btnic.cgi")
 
     # we create two windows - one for the HLT and one for the MLT
-    hlt = Tun(1, 10,10)
-    hlt.update()
-    time.sleep(20)
-    
+    hlt = Tun(bt,0, "HLT", 10,10)
+    mlt = Tun(bt,1, "MLT", 10,35)
+    pump = Pump(21,10,14,13)
+
+
+    while 1:
+        hlt.update()
+        mlt.update()
+        pump.update()
+        time.sleep(1)
 
 curses.wrapper(main)
--- brewtroller.py
+++ brewtroller.py
@@ -1,16 +1,29 @@
 import requests
 
+# commands: https://github.com/BrewTroller/BrewTroller-Official/blob/master/Com_BTnic.h#L557
+
 class BrewTroller:
     def __init__(self, adress):
         self.adress=adress
 
     # use a http request to send a command
     # parse the return value
-    def sendCommand(command):
-        r = requests.get(adress,params={command})
-
+    def sendCommand(self,command):
+        r = requests.get(self.adress+"?"+command)
+        # parse the request into an array of responses
+        return str(r.text).translate(None,"[]\"").split(",")
     
     # get the temperature of a tun
-    def getTemp(tunid):
+    def getTemp(self,tunid):
         response=self.sendCommand("q"+str(tunid))
-        return response[0]
+        return round(float(response[2])/100,2)
+
+    # get the setpoint of a tun
+    def getSetpoint(self,tunid):
+        response=self.sendCommand("t"+str(tunid))
+        return round(float(response[2]),2) # this does not seem to be multiplied with 100
+
+    # get the heating power of a tun
+    def getHeatpwr(self,tunid):
+        response=self.sendCommand("s"+str(tunid))
+        return round(float(response[2]),0) # this does not seem to be multiplied with 100
